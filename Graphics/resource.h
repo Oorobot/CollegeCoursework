bdb68e4e352776d684d703036ec880138c7bd280
zerobe0 <1069980147@qq.com>
2020-05-08 21:53:32 +0800

    合并

---
+++ Graphics/resource.h
@@ -0,0 +1,59 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Graphics.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_GRAPH_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SMALL                       108
+#define IDC_GRAPHICS                    109
+#define IDR_MAINFRAME                   128
+#define IDD_LBOX                        129
+#define IDD_CBOX                        130
+#define IDD_PRZ                         137
+#define IDI_GRAPHICS                    140
+#define ID_Y0                           1004
+#define ID_Y1                           1005
+#define IDC_CA                          1007
+#define IDC_CB                          1008
+#define IDC_CR                          1009
+#define IDC_X                           1025
+#define IDC_Y                           1026
+#define IDC_PanX                        1027
+#define IDC_PanY                        1028
+#define IDC_RotateA                     1029
+#define IDC_ZoomX                       1030
+#define IDC_EDIT7                       1031
+#define IDC_ZoomY                       1031
+#define ID_CIRCLE                       32775
+#define ID_DDA                          32776
+#define ID_MID                          32777
+#define ID_BRE                          32778
+#define ID_X1                           32779
+#define ID_X0                           32780
+#define ID_M_ISOTROPIC                  32794
+#define ID_M_TEXT                       32795
+#define ID_M_ANISOTROPIC                32796
+#define ID_M_LOMETRIC                   32797
+#define ID_M_HIMETRIC                   32798
+#define ID_M_LOENGLISH                  32799
+#define ID_M_HENGGLIST                  32800
+#define ID_M_TWIPS                      32801
+#define ID_M_HIENGLISH                  32802
+#define ID_PRZ                          32804
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32806
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
